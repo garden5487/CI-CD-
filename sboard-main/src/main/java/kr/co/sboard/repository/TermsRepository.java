be6a6cd6d68758309a8ae30243da95499a087e1e
unknown <gardenpark9001@gmail.com>
2025-06-30 16:39:38 +0900

    베포 테스트

--- sboard-main/src/main/java/kr/co/sboard/repository/TermsRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.co.sboard.repository;
-
-import kr.co.sboard.dto.TermsDTO;
-import kr.co.sboard.entity.Terms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TermsRepository extends JpaRepository<Terms, Integer> {
-}
